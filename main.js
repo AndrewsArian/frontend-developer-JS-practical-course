551f2e3e55de525b980496338509417aabda96c0
AndrewArian <abradan97@gmail.com>
2023-01-15 17:07:18 -0300

    Integration of shoppingkart menu,from the class 13

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuKartIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuKartIcon.addEventListener('click', toggleKartAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
-    }
+}
+
+function toggleKartAside(){
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
+}
